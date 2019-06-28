3f48c3d8e47a80998aa4d31529b7fdf27aa2dd0c
dyuthichand <dyuthichand@gmail.com>
2019-06-28 10:21:22 +0530

    Updates

---
+++ precache-manifest.cc11f3ba36b0b3bb0d18753b9b0e1455.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a97e03c4d7242780144b276ca919b42c",
+    "url": "/Weather-Forcast/index.html"
+  },
+  {
+    "revision": "b2e7d96c5a93682f8bed",
+    "url": "/Weather-Forcast/static/css/main.0bafc90a.chunk.css"
+  },
+  {
+    "revision": "8afb82321dd4ec092eaa",
+    "url": "/Weather-Forcast/static/js/2.720c356b.chunk.js"
+  },
+  {
+    "revision": "b2e7d96c5a93682f8bed",
+    "url": "/Weather-Forcast/static/js/main.c1a61980.chunk.js"
+  },
+  {
+    "revision": "3da6454afb1e2c7e8946",
+    "url": "/Weather-Forcast/static/js/runtime~main.6783452c.js"
+  }
+]);
